a142561f8510fb8d7d7016ebb53588339237648e
Erik <bai@halfhelix.com>
2020-01-08 13:09:59 +0800

    Update

---
+++ dist/precache-manifest.1766146b9a7c046ae698d7cf3e1f0043.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a775e2d9d76bf6da9decf0746087cc5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f10ed1e4b14b2d7105f",
+    "url": "/static/css/2.9d04c5f8.chunk.css"
+  },
+  {
+    "revision": "92307e8252738c0cc912",
+    "url": "/static/css/main.f4261c90.chunk.css"
+  },
+  {
+    "revision": "1f10ed1e4b14b2d7105f",
+    "url": "/static/js/2.c0fb8571.chunk.js"
+  },
+  {
+    "revision": "92307e8252738c0cc912",
+    "url": "/static/js/main.5bc4c2cd.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
